16b10c42c7107d3c5fed078c2bd88ce2eeddd95c
Erel Segal-Halevi <erelsgl@gmail.com>
2025-06-18 13:56:15 +0300

    copy from moodle

---
+++ sums.py
@@ -0,0 +1,7 @@
+# Put your iterator/generator here. 
+# It should be called: sorted_subset_sums.
+
+if __name__ == '__main__':
+    from itertools import takewhile, islice
+    for i in eval(input()):
+        print(i, end=", ")
